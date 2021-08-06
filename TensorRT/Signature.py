8556566a4ddf3d67304332bf8d2c817da85175f4
Yavuz BALI <84620286+YavuzBALI@users.noreply.github.com>
2021-08-06 17:43:21 +0300

    Update Signature.py

--- TensorRT/Signature.py
+++ TensorRT/Signature.py
@@ -1,11 +1,17 @@
 import tensorflow as tf 
 from tensorflow.python.compiler.tensorrt import trt_convert as trt
 
+#Registered model address is entered
 output_saved_model="/home/yavuz/OpenCv2Tensorflow_RobocupAngle/Robot_Angle_pre_SavedModel_101x101_FP16_build_2"
+#Load Model
 saved_model_loaded=tf.saved_model.load(output_saved_model,)
+#The model signature key is taken.
 signature_keys= list(saved_model_loaded.signatures.keys())
+#Printing signatur key
 print(signature_keys)
 
+#The model object is retrieved.
 infer =saved_model_loaded.signatures['serving_default']
+#Printing model object
 print(infer.structured_outputs)
 
